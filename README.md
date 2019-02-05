81d882d410582087d4d7677528a0bf1b544321ed
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-02-05 11:36:31 +0000

    Updated for pypi release

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-Quantiphyse plugin for FSL command line tools
+FSL plugin for Quantiphyse
+==========================
 
-See https://quantiphyse.readthedocs.io/ Quantiphyse documentation
+This plugin provides a Quantiphyse interface to selected FSL
+tools, currently:
+
+  - BET
+  - FAST
+  - FLIRT/MCFLIRT (via the registration widget)
+  - FSL_ANAT (experimental)
+  - Atlasses/standard data
+
+Quantiphyse and FSL must be installed. Note that this plugin is
+an interface only and contains no FSL code itself!
+To install the plugin from PyPi use:
+
+    pip install quantiphyse-fsl
+
+The plugin will then be available from within Quantiphyse
+
--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import os
 import subprocess
 import re
 import io
+import glob
 
 from setuptools import setup
 from setuptools import find_packages
@@ -22,11 +23,11 @@ def git_version():
         # Full version includes the Git commit hash
         full_version = subprocess.check_output('git describe --dirty', shell=True).decode("utf-8").strip(" \n")
 
-        # Python standardized version in form major.minor.patch.dev<build>
+        # Python standardized version in form major.minor.patch.post<build>
         version_regex = re.compile(r"v?(\d+\.\d+\.\d+(-\d+)?).*")
         match = version_regex.match(full_version)
         if match:
-            std_version = match.group(1).replace("-", ".dev")
+            std_version = match.group(1).replace("-", ".post")
         else:
             raise RuntimeError("Failed to parse version string %s" % full_version)
         return full_version, std_version
@@ -71,6 +72,14 @@ def get_version(rootdir):
                 version = "unknown"
     return version
 
+def get_package_data(rootdir):
+    """
+    Get extra data files to install into the package, e.g. icons
+    """
+    return {
+        MODULE : glob.glob("%s/*.png" % MODULE) + glob.glob("%s/*.svg" % MODULE)
+    }
+
 module_dir = os.path.abspath(os.path.dirname(__file__))
 
 kwargs = {
@@ -85,6 +94,8 @@ kwargs = {
     'license' : 'License granted by University of Oxford for use by academics carrying out research and not for use by consumers or commercial businesses. See LICENSE file for more details',
     'install_requires' : get_requirements(module_dir),
     'packages' : find_packages(),
+    'package_data' : get_package_data(module_dir),
+    'include_package_data' : True,
     'entry_points' : {
         'quantiphyse_plugins' : [
             '%s = %s:QP_MANIFEST' % (MODULE, MODULE),
